f006ca4abbb302665e9b8edd676b538fe0ed15b1
Michel Albert <michel@albert.lu>
2014-01-30 10:39:00 +0100

    Simplified the SSDP discovery test.

--- fritzclient/tests/test_ssdp.py
+++ fritzclient/tests/test_ssdp.py
@@ -17,16 +17,6 @@ class TestDiscovery(TestCase):
         sock.recv.return_value = example_response
         response = discover()
 
-        disco_message = (
-            b'M-SEARCH * HTTP/1.1\n'
-            b'Host: 239.255.255.250:1900\n'
-            b'Man: "ssdp:discover"\n'
-            b'MX: 5\n'
-            b'ST: urn:dslforum-org:device:InternetGatewayDevice:1\n'
-        )
-
-        sock.sendto(disco_message, ('239.255.255.250', 1900))
-
         expected = {
             'status_code': 200,
             'location': 'http://192.168.179.1:49000/tr64desc.xml',
