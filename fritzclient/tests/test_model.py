336e07e85946c0625d9b32d68516baad0bfb5cf3
Michel Albert <michel@albert.lu>
2014-02-09 12:09:06 +0100

    Moving to a more dynamic structure.
    I'm still trying to decide on the external API while wrapping my head
    around TR064. Things are shifting like crazy...
    
    Added all XML documents available from the current Fritz-Box for
    testing.

--- fritzclient/tests/test_model.py
+++ fritzclient/tests/test_model.py
@@ -1,7 +1,7 @@
 from contextlib import nested
 from os import urandom
 from pkg_resources import resource_string
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from mock import patch
 
@@ -18,8 +18,8 @@ class BaseTest(TestCase):
         tr064desc = resource_string('fritzclient',
                                     'tests/data/tr64desc.xml')
         with nested(patch('fritzclient.model.tr064'),
-                    patch('fritzclient.model.requests')) as (mock_tr064,
-                                                             mock_req):
+                    patch('fritzclient.model.minisoap')) as (mock_tr064,
+                                                             mock_soap):
             mock_tr064.get_tr064desc.return_value = tr064desc
             mock_tr064.discover.return_value = {
                 'status_code': 200,
@@ -32,7 +32,7 @@ class BaseTest(TestCase):
                 'usn': ('uuid:739f2409-bccb-40e7-8e6c-0024FE6E00C3::'
                         'urn:dslforum-org:device:InternetGatewayDevice:1'),
             }
-            self.igd = mdl.InternetGatewayDevice()
+            self.dev = mdl.InternetGatewayDevice()
 
 
 class TestInternetGatewayDeviceDiscovery(BaseTest):
@@ -59,46 +59,213 @@ class TestInternetGatewayDeviceDiscovery(BaseTest):
         self.assertEqual(igd.spec_version, '1.0')
 
 
-class TestInternetGatewayDevice(BaseTest):
+class TestDevice(BaseTest):
+
+    def test_services(self):
+        expected = set([
+            {
+                'service_type': 'urn:dslforum-org:service:DeviceInfo:1',
+                'service_id': 'urn:DeviceInfo-com:serviceId:DeviceInfo1',
+                'control_url': '/upnp/control/deviceinfo',
+                'event_sub_url': '/upnp/control/deviceinfo',
+                'scpd_url': '/deviceinfoSCPD.xml',
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:DeviceConfig:1',
+                'service_id': 'urn:DeviceConfig-com:serviceId:DeviceConfig1',
+                'control_url': '/upnp/control/deviceconfig',
+                'event_sub_url': '/upnp/control/deviceconfig',
+                'scpd_url': '/deviceconfigSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:Layer3Forwarding:1',
+                'service_id': ('urn:Layer3Forwarding-com:serviceId:'
+                               'Layer3Forwarding1'),
+                'control_url': '/upnp/control/layer3forwarding',
+                'event_sub_url': '/upnp/control/layer3forwarding',
+                'scpd_url': '/layer3forwardingSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:LANConfigSecurity:1',
+                'service_id': ('urn:LANConfigSecurity-com:serviceId:'
+                               'LANConfigSecurity1'),
+                'control_url': '/upnp/control/lanconfigsecurity',
+                'event_sub_url': '/upnp/control/lanconfigsecurity',
+                'scpd_url': '/lanconfigsecuritySCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:ManagementServer:1',
+                'service_id': ('urn:ManagementServer-com:serviceId:'
+                               'ManagementServer1'),
+                'control_url': '/upnp/control/mgmsrv',
+                'event_sub_url': '/upnp/control/mgmsrv',
+                'scpd_url': '/mgmsrvSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:Time:1',
+                'service_id': 'urn:Time-com:serviceId:Time1',
+                'control_url': '/upnp/control/time',
+                'event_sub_url': '/upnp/control/time',
+                'scpd_url': '/timeSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:UserInterface:1',
+                'service_id': 'urn:UserInterface-com:serviceId:UserInterface1',
+                'control_url': '/upnp/control/userif',
+                'event_sub_url': '/upnp/control/userif',
+                'scpd_url': '/userifSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:X_VoIP:1',
+                'service_id': 'urn:X_VoIP-com:serviceId:X_VoIP1',
+                'control_url': '/upnp/control/x_voip',
+                'event_sub_url': '/upnp/control/x_voip',
+                'scpd_url': '/x_voipSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:X_AVM-DE_Storage:1',
+                'service_id': ('urn:X_AVM-DE_Storage-com:serviceId:'
+                               'X_AVM-DE_Storage1'),
+                'control_url': '/upnp/control/x_storage',
+                'event_sub_url': '/upnp/control/x_storage',
+                'scpd_url': '/x_storageSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:X_AVM-DE_OnTel:1',
+                'service_id': ('urn:X_AVM-DE_OnTel-com:serviceId:'
+                               'X_AVM-DE_OnTel1'),
+                'control_url': '/upnp/control/x_contact',
+                'event_sub_url': '/upnp/control/x_contact',
+                'scpd_url': '/x_contactSCPD.xml'
+            },
+            {
+                'service_type': ('urn:dslforum-org:service:'
+                                 'X_AVM-DE_WebDAVClient:1'),
+                'service_id': ('urn:X_AVM-DE_WebDAV-com:serviceId:'
+                               'X_AVM-DE_WebDAVClient1'),
+                'control_url': '/upnp/control/x_webdav',
+                'event_sub_url': '/upnp/control/x_webdav',
+                'scpd_url': '/x_webdavSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:X_AVM-DE_UPnP:1',
+                'service_id': 'urn:X_AVM-DE_UPnP-com:serviceId:X_AVM-DE_UPnP1',
+                'control_url': '/upnp/control/x_upnp',
+                'event_sub_url': '/upnp/control/x_upnp',
+                'scpd_url': '/x_upnpSCPD.xml'
+            },
+            {
+                'service_type': ('urn:dslforum-org:service:'
+                                 'X_AVM-DE_RemoteAccess:1'),
+                'service_id': ('urn:X_AVM-DE_RemoteAccess-com:serviceId:'
+                               'X_AVM-DE_RemoteAccess1'),
+                'control_url': '/upnp/control/x_remote',
+                'event_sub_url': '/upnp/control/x_remote',
+                'scpd_url': '/x_remoteSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:X_AVM-DE_MyFritz:1',
+                'service_id': ('urn:X_AVM-DE_MyFritz-com:serviceId:'
+                               'X_AVM-DE_MyFritz1'),
+                'control_url': '/upnp/control/x_myfritz',
+                'event_sub_url': '/upnp/control/x_myfritz',
+                'scpd_url': '/x_myfritzSCPD.xml'
+            },
+            {
+                'service_type': 'urn:dslforum-org:service:X_AVM-DE_TAM:1',
+                'service_id': 'urn:X_AVM-DE_TAM-com:serviceId:X_AVM-DE_TAM1',
+                'control_url': '/upnp/control/x_tam',
+                'event_sub_url': '/upnp/control/x_tam',
+                'scpd_url': '/x_tamSCPD.xml'
+            }])
+
+        result = [{
+            'service_type': _._service_type,
+            'service_id': _._service_id,
+            'control_url': _._control_url,
+            'event_sub_url': _._event_sub_url,
+            'scpd_url': _._scpd_url
+        } for _ in self.dev.services]
+
+        self.assertEqual(result, expected)
+
+    def test_devices(self):
+        pass
 
     def test_l3fw(self):
-        self.assertIsInstance(self.igd.layer3_forwarding,
+        self.assertIsInstance(self.dev.layer3_forwarding,
                               mdl.Layer3Forwarding)
 
     def test_device_info(self):
-        self.assertIsInstance(self.igd.device_info,
+        self.assertIsInstance(self.dev.device_info,
                               mdl.DeviceInfo)
 
     def test_device_config(self):
-        self.assertIsInstance(self.igd.device_config,
+        self.assertIsInstance(self.dev.device_config,
                               mdl.DeviceConfig)
 
     def test_lan_config_security(self):
-        self.assertIsInstance(self.igd.lan_config_security,
+        self.assertIsInstance(self.dev.lan_config_security,
                               mdl.LANConfigSecurity)
 
     def test_management_server(self):
-        self.assertIsInstance(self.igd.management_server,
+        self.assertIsInstance(self.dev.management_server,
                               mdl.ManagementServer)
 
     def test_time(self):
-        self.assertIsInstance(self.igd.time,
+        self.assertIsInstance(self.dev.time,
                               mdl.Time)
 
     def test_user_interface(self):
-        self.assertIsInstance(self.igd.user_interface,
+        self.assertIsInstance(self.dev.user_interface,
                               mdl.UserInterface)
 
     def test_lan_device(self):
-        self.assertIsInstance(self.igd.lan_device,
+        self.assertIsInstance(self.dev.lan_device,
                               mdl.LANDevice)
+
+    def test_wan_device(self):
+        self.assertIsInstance(self.dev.lan_device,
+                              mdl.WANDevice)
+
+    @skip('AVM Extensions are not yet implemented')
+    def test_X_VoIP(self):
+        self.fail('not yet implemented')
+
+    @skip('AVM Extensions are not yet implemented')
+    def test_X_AVM_DE_Storage(self):
+        self.fail('not yet implemented')
+
+    @skip('AVM Extensions are not yet implemented')
+    def test_X_AVM_DE_OnTel(self):
+        self.fail('not yet implemented')
+
+    @skip('AVM Extensions are not yet implemented')
+    def test_X_AVM_DE_WebDAVClient(self):
+        self.fail('not yet implemented')
+
+    @skip('AVM Extensions are not yet implemented')
+    def test_X_AVM_DE_UPnP(self):
+        self.fail('not yet implemented')
+
+    @skip('AVM Extensions are not yet implemented')
+    def test_X_AVM_DE_RemoteAccess(self):
+        self.fail('not yet implemented')
+
+    @skip('AVM Extensions are not yet implemented')
+    def test_X_AVM_DE_MyFritz(self):
+        self.fail('not yet implemented')
+
+    @skip('AVM Extensions are not yet implemented')
+    def test_X_AVM_DE_TAM(self):
+        self.fail('not yet implemented')
 
 
 class TestDeviceInfo(BaseTest):
 
     def setUp(self):
         super(TestDeviceInfo, self).setUp()
-        self.info = self.igd.device_info
+        self.info = self.dev.device_info
 
     def test_wan_device(self):
         self.assertIsInstance(self.info.wan_device,
@@ -110,63 +277,58 @@ class TestDeviceInfo(BaseTest):
     def test_manufacturer_oui(self):
         oui = self.info.manufacturer_oui
         self.assertEqual(len(oui), 6)
-        self.assertEqual(oui, UNDEF)
+        self.assertEqual(oui, '00040E')
 
     def test_model_name(self):
         self.assertEqual(self.info.model_name,
-                         UNDEF)
+                         'FRITZ!Box Fon WLAN 7390')
 
     def test_description(self):
         self.assertEqual(self.info.description,
-                         UNDEF)
+                         'FRITZ!Box Fon WLAN 7390 84.05.52')
 
     def test_product_class(self):
         self.assertEqual(self.info.product_class,
-                         UNDEF)
+                         'FRITZ!Box')
 
     def test_serial_number(self):
         self.assertEqual(self.info.serial_number,
-                         UNDEF)
+                         '0896D74C6BF8')
 
     def test_software_version(self):
         self.assertEqual(self.info.software_version,
-                         UNDEF)
+                         '84.05.52')
 
     def test_additional_software_versions(self):
-        self.assertEqual(self.info.additional_software_versions,
-                         UNDEF)
+        self.assertIsNone(self.info.additional_software_versions)
 
     def test_modem_firmware_version(self):
-        self.assertEqual(self.info.modem_firmware_version,
-                         UNDEF)
+        self.assertIsNone(self.info.modem_firmware_version)
 
     def test_enabled_options(self):
-        self.assertEqual(self.info.enabled_options,
-                         UNDEF)
+        self.assertIsNone(self.info.enabled_options)
 
     def test_hardware_version(self):
         self.assertEqual(self.info.hardware_version,
-                         UNDEF)
+                         'FRITZ!Box Fon WLAN 7390')
 
     def test_additional_hardware_versions(self):
-        self.assertEqual(self.info.additional_hardware_versions,
-                         UNDEF)
+        self.assertIsNone(self.info.additional_hardware_versions)
 
     def test_spec_version(self):
         self.assertEqual(self.info.spec_version,
-                         UNDEF)
+                         '1.0')
 
     def test_provisioning_code(self):
         self.assertEqual(self.info.provisioning_code,
-                         UNDEF)
+                         '7390V2012.09.19')
 
     def test_uptime(self):
         self.assertEqual(self.info.uptime,
-                         UNDEF)
+                         '794051')
 
     def test_first_use_date(self):
-        self.assertEqual(self.info.first_use_date,
-                         UNDEF)
+        self.assertIsNone(self.info.first_use_date)
 
     def test_device_log(self):
         self.assertEqual(self.info.device_log,
@@ -206,3 +368,6 @@ class TestDeviceInfo(BaseTest):
             'device_log': UNDEF
         }
         self.assertEqual(result, expected)
+
+
+# vim: set path+=fritzclient :
